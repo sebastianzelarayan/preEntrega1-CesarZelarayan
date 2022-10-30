dc827b911d7dc6c637cdc178d0e17243c41b5998
Sebastian Zelarayan <cesarszelarayan-29@hotmail.com>
2022-10-30 18:17:35 -0300

    Agregado de funciones constructoras en catalogos productos

--- PreEntregaZelarayan.js
+++ PreEntregaZelarayan.js
@@ -1,48 +1,48 @@
-// let titulo = "Espartano $ 12.999"
-// console.log(titulo)
+let titulo = "Espartano $ 12.999"
+console.log(titulo)
 
-// let titulo1 = "Torsion $ 13.999"
-// console.log(titulo1)
+let titulo1 = "Torsion $ 13.999"
+console.log(titulo1)
 
-// let titulo2 = "Mecanico $ 15.999"
-// console.log(titulo2)
+let titulo2 = "Mecanico $ 15.999"
+console.log(titulo2)
 
-// let titulo3 = "Color Blanco y Negro"
-// console.log(titulo3)
+let titulo3 = "Color Blanco y Negro"
+console.log(titulo3)
 
-// let titulo4 = "Color Cobre y Negro"
-// console.log(titulo4)
+let titulo4 = "Color Cobre y Negro"
+console.log(titulo4)
 
-// let titulo5 = "Material Pla"
-// console.log(titulo5)
+let titulo5 = "Material Pla"
+console.log(titulo5)
 
-// let titulo6 = "Acabado Brillante"
-// console.log(titulo6)
+let titulo6 = "Acabado Brillante"
+console.log(titulo6)
 
-// let titulo7 = "Acabado Satinado"
-// console.log(titulo7)
+let titulo7 = "Acabado Satinado"
+console.log(titulo7)
 
-// let titulo8 = "Acabado Brillante"
-// console.log(titulo8)
+let titulo8 = "Acabado Brillante"
+console.log(titulo8)
 
 
-// let preguntarEdad = prompt("Responder por si o por no, si es mayor de edad")
+let preguntarEdad = prompt("Responder por si o por no, si es mayor de edad")
 
-// if(preguntarEdad.toLowerCase() == "si"){
-//     console.log("Usted puede comprar nuestros productos")
-// }else{
-//     console.log("Usted NO puede comprar nuestros productos")
-// }
+if(preguntarEdad.toLowerCase() == "si"){
+    console.log("Usted puede comprar nuestros productos")
+}else{
+    console.log("Usted NO puede comprar nuestros productos")
+}
 
-// let colorDeseado = prompt("Ingrese color deseado")
+let colorDeseado = prompt("Ingrese color deseado")
 
-// if(colorDeseado.toLowerCase() == "blanco y negro"){
-//     console.log("Usted eligio color " + colorDeseado)
-// }else if(colorDeseado == "cobre y negro"){
-//     console.log("Usted eligio color " + colorDeseado)
-// }else{
-//     console.log("NO trabajamos ese color")
-// }
+if(colorDeseado.toLowerCase() == "blanco y negro"){
+    console.log("Usted eligio color " + colorDeseado)
+}else if(colorDeseado == "cobre y negro"){
+    console.log("Usted eligio color " + colorDeseado)
+}else{
+    console.log("NO trabajamos ese color")
+}
 
 function pedirNombreJuego(){
     let nombreJuego = prompt("Ingrese nombre de juego")
@@ -68,46 +68,46 @@ if(mul){
     console.log("Si no elige una cantidad, usted no puede comprar nuestros productos")
 }
 
-// function pedirNombre(){
-//     let personaIngresada = prompt("Ingresar nombre")
-//     console.log(`Hola ${personaIngresada} bienvenido a Ziddesign`)
-// }
+function pedirNombre(){
+    let personaIngresada = prompt("Ingresar nombre")
+    console.log(`Hola ${personaIngresada} bienvenido a Ziddesign`)
+}
 
-// pedirNombre()
+pedirNombre()
 
-// function suma(juegoEspartano, juegoEspartano, juegoTorsion){
-//     console.log(`La suma es $ ${juegoEspartano+juegoEspartano+juegoTorsion} `)
-// }
-// suma(12999,12999,13999)
+function suma(juegoEspartano, juegoEspartano, juegoTorsion){
+    console.log(`La suma es $ ${juegoEspartano+juegoEspartano+juegoTorsion} `)
+}
+suma(12999,12999,13999)
 
-// function resta(juegoTorsion, juegoMecanico){
-//     return juegoTorsion-juegoMecanico
-// }
-// let resultado = resta(15999,13999)
-// console.log(resultado)
+function resta(juegoTorsion, juegoMecanico){
+    return juegoTorsion-juegoMecanico
+}
+let resultado = resta(15999,13999)
+console.log(resultado)
 
-// function pedirValores(){
-//     let valor1 = parseInt(prompt("Ingrese el primer valor"))
-//     let valor2 = parseInt(prompt("Ingrese el segundo valor"))
-//     let operador = prompt("Ingrese el operador deseado")
-//     let resultado = calculadora(valor1, valor2, operador)
-//     console.log(resultado)
-// }
+function pedirValores(){
+    let valor1 = parseInt(prompt("Ingrese el primer valor"))
+    let valor2 = parseInt(prompt("Ingrese el segundo valor"))
+    let operador = prompt("Ingrese el operador deseado")
+    let resultado = calculadora(valor1, valor2, operador)
+    console.log(resultado)
+}
 
-// function calculadora(juegoMecanico, primerPuesto, operacion){
-//     switch (operacion) {
-//         case "+":
-//             return juegoMecanico + primerPuesto;
-//         case "-":
-//             return juegoMecanico - primerPuesto;
-//         default:
-//             return 0;
-//     }
-// }
-// console.log(calculadora(15999, 17999, "+"))
-// console.log(calculadora(15999, 17999, "-"))
+function calculadora(juegoMecanico, primerPuesto, operacion){
+    switch (operacion) {
+        case "+":
+            return juegoMecanico + primerPuesto;
+        case "-":
+            return juegoMecanico - primerPuesto;
+        default:
+            return 0;
+    }
+}
+console.log(calculadora(15999, 17999, "+"))
+console.log(calculadora(15999, 17999, "-"))
 
-// pedirValores ()
+pedirValores ()
 
 const integrante1 = {
     nombre: "Sebastian",
